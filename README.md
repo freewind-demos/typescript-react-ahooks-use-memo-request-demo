bbeac5d0d799920ada2ce710e17b1d9398e3f180
Freewind <nowindlee@gmail.com>
2022-09-24 16:34:39 +0800

    typescript-react-ahooks-use-memo-request-demo

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-TypeScript React Ahooks useRequest Demo
+TypeScript React Ahooks useMemoRequest Demo
 =================================
 
-使用ahooks的useRequest来发送异步请求
+为了方便使用，最好对 `useRequest`进行包装，只有当状态变化时，才返回变化的对象，而不是每次render都生成新对象。
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
 It will open page on browser automatically.
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,29 +1,27 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './Hello.pcss';
-import {useRequest} from "ahooks";
+import {useMemoRequest} from "./useMemoRequest";
 
 type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
     const [version, setVersion] = useState(0)
 
-    const request = useRequest(async () => {
+    const request = useMemoRequest(async () => {
         console.log("### version", version)
-        return new Promise<number>(resolve => {
-            setTimeout(() => {
-                resolve(version);
-            }, 1000)
-        })
+        return Promise.resolve(version)
     })
+
+    useEffect(() => {
+        console.log('### request is changing')
+    }, [request]);
+
     return <div className={'Hello'}>
         <h1>Hello React</h1>
         <div>
             <div>
-                <button onClick={() => {
-                    setVersion(n => n + 1);
-                    request.refresh()
-                }}>update
-                </button>
+                <button onClick={() => setVersion(n => n + 1)}>update version ({version})</button>
+                <button onClick={() => request.refresh()}>refetch</button>
             </div>
             <div>request:
                 <ul>
